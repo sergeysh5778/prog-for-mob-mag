7e6dfb7cdee8f1b3fec88b751054601029902008
Широкобородов Сергей Владимирович <sergeysh@sfedu.ru>
2022-05-10 16:17:51 +0300

    Lab 2: Plugins and dependencies update

--- Lab2/build.gradle.kts
+++ Lab2/build.gradle.kts
@@ -11,9 +11,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.3.0-alpha09" apply false
-    id("com.android.library") version "7.3.0-alpha09" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.20-M1" apply false
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
